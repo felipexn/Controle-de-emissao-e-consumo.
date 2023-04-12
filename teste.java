6a42a730bde33cecdbbf505222af266dbea71f8e
Luiz Felipe <108966595+felipexn@users.noreply.github.com>
2023-04-11 22:53:36 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 public class teste {
     public static void main(String[] args) {
         ArrayList<Onibus> onibus = new ArrayList();
-        onibus.add(new OnibusGasolina(1, 7000));
-        onibus.add(new OnibusEletrico(2,5000,4000));
-        onibus.add(new OnibusHibrido(3,7000,5000));
+        onibus.add(new OnibusGasolina(1, 5000));
+        onibus.add(new OnibusEletrico(2,5000,300));
+        onibus.add(new OnibusHibrido(3,7000,300));
         
         
         for(Onibus it:onibus){
